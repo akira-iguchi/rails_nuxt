c8cca4a9e15d562642c24d250374952feed358b9
Akira Iguchi <akituri931@icloud.com>
2022-01-24 23:19:41 +0900

    add 削除機能

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,7 +2,7 @@
   <div v-if="user">
     <p>名前：{{user.name}}</p>
     <AddTodo @submit="addTodo" />
-    <TodoList :todos="todos" />
+    <TodoList :todos="user.todos" />
   </div>
 </template>
 
@@ -35,10 +35,16 @@
       }
     },
     methods: {
-      async addTodo(title) {
-        await axios.post("/v1/todos", { title });
-        this.todos.push({
-          title
+      async addTodo(todo) {
+        const { data } = await axios.post("/v1/todos", { todo });
+
+        this.$store.dispatch("auth/setUser", {
+          // スプレッド構文で展開。userの情報とTODOの情報をまとめて綺麗に1個のオブジェクトにして、Vuexのactionsに渡す
+          // 例：{id: 1, name: 'テストユーザー', email: 't@nuxt.rails.todoapp', todos: Array(2)}で、user.idやuser.todosと言う形で呼び出せる
+          // スプレッド構文で展開しないと、{user: {id: 1, name: 'テストユーザー', email: 't@nuxt.rails.todoapp'}, todos: Array(2)}のようになり、
+          // IDを呼び出したい際などは、user.user.idのようになってしまう
+          ...this.user,
+          todos: [...this.user.todos, data]
         });
       },
     },
